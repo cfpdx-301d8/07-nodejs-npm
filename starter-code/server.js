aa5cfedc526976f682b035347028d22ba3746544
Jane Smith <example@email.com>
2017-03-14 13:30:38 -0700

    completed the first three TODOs and the last consol.log

--- starter-code/server.js
+++ starter-code/server.js
@@ -1,15 +1,17 @@
 'use strict';
 
-// TODO: Initialize your project using NPM to create and populate a package.json file
+// TODO: Initialize your project using NPM to create and populate a package.json file...DONE
 
 // TODO: Require the Express package that you installed via NPM, and instantiate the app
 // Remember to install express, and be sure that it's been added to your package.json as a dependency
-
+const express = require('express');
+const app = express();
 const PORT = process.env.PORT || 3000;
 
 // TODO: Include all of the static resources as an argument to app.use()
-
+app.use(express.static('./public'))
 app.get('*', function(request, response) {
+  
   // TODO: Using the response object, send the index.html file back to the user
 });
 
@@ -17,4 +19,5 @@ app.get('*', function(request, response) {
 
 app.listen(PORT, function() {
   // TODO: Log to the console a message that lets you know which port your server has started on
+  console.log(`server up on ${PORT}`)
 });
