41f22b964a3cc7d1a871f9f0fa5f2b081163e5fb
Keeley Hammond <keeleymhammond@gmail.com>
2017-03-14 13:47:50 -0700

    Lab complete

--- starter-code/server.js
+++ starter-code/server.js
@@ -1,20 +1,32 @@
 'use strict';
 
-// TODO: Initialize your project using NPM to create and populate a package.json file
-
-// TODO: Require the Express package that you installed via NPM, and instantiate the app
+// DONE TODO: Initialize your project using NPM to create and populate a package.json file
+// DONE TODO: Require the Express package that you installed via NPM, and instantiate the app
 // Remember to install express, and be sure that it's been added to your package.json as a dependency
+const express = require('express');
+const app = express();
 
+// The process object is a global that's describing the current process - process.env returns an object
+// that contains the user's environment (is this a container, like Docker?). 
+// Is this line assigning a port and a port property to the user's environment?
+// This line is stating an "OR" - either use the user environment's port or Port 3000 if one doesn't exist.
 const PORT = process.env.PORT || 3000;
+console.log(process.env);
 
-// TODO: Include all of the static resources as an argument to app.use()
+// DONE TODO: Include all of the static resources as an argument to app.use()
+app.use(express.static('./public'));
 
 app.get('*', function(request, response) {
-  // TODO: Using the response object, send the index.html file back to the user
+  // DONE TODO: Using the response object, send the index.html file back to the user
+  response.sendFile('/public/index.html', {root: '.'})
 });
 
 // TODO: (STRETCH) Write a new route that will handle a request and send the new.html file back to the user
+app.get('/new.html', function(request, response) {
+  response.sendFile('/public/new.html', {root: '.'})
+});
 
 app.listen(PORT, function() {
-  // TODO: Log to the console a message that lets you know which port your server has started on
+  // DONE TODO: Log to the console a message that lets you know which port your server has started on
+  console.log(`Server up on ${PORT}`)
 });
